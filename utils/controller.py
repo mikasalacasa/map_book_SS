1b310f789dbc9ade2e3e709c14fafd95db28df12
student <szsendrowski@gmail.com>
2025-04-09 14:49:19 +0200

    wip: added remove user functionality

--- utils/controller.py
+++ utils/controller.py
@@ -1,6 +1,6 @@
-def getUserInfo (usersData:list)->None:
+def get_user_info (usersData:list)->None:
     for user in usersData:
-        print(f'Twój znajomy {user["name"]} z miejscowości {user["location"]}')\
+        print(f'Twój znajomy {user["name"]} z miejscowości {user["location"]}')
 
 def add_user(users_data:list)->None:
 
@@ -9,3 +9,9 @@ def add_user(users_data:list)->None:
     new_posts:int = int(input("Podaj jego liczbę postów "))
 
     users_data.append({"name":new_name,"location":new_location,"posts":new_posts})
+
+def delete_user(users_data:list)->None:
+    username:str = input("Podaj imie znajomego którego chcesz usunąć: ")
+    for user in users_data:
+        if user["name"] == username:
+            users_data.remove(user)
